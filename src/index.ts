3886275134157fe3f274ce1cb1407f032cd61eba
Tyler Grove <itylerx@me.com>
2024-12-02 09:38:13 +0200

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { API } from 'homebridge';
 import { BluetoothLEDPlatform } from './platform';  // Update import to the correct class name
 
-
 import { PLUGIN_NAME, PLATFORM_NAME } from './settings';
 
 // Default export of the platform
